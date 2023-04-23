24356758f2183f66fef5ae759b0a6d0e02fe050c
jaimereyesr <jaime.reyesr22@gmail.com>
2023-04-23 17:23:56 -0600

    first commit

---
+++ src/logic/board.js
@@ -0,0 +1,22 @@
+import { WINNER_COMBOS } from "../constanst";
+
+export const checkWinnerFrom = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+        ) {
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+  }
+
+  export const checkEndGame = (newBoard) => {
+    // revisamos si hay un empate
+    // si no hay más espacios vacíos
+    // en el tablero
+    return newBoard.every((square) => square !== null)
+  }
